ed2873cffea2772bd5ed96d6b08dbbcd70b9627c
Tim Ruel <timruel88@gmail.com>
2026-01-21 19:11:19 -0600

    ci: declare external imports via roxygen

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,12 @@
 #' @importFrom stats optim rlnorm rnorm runif
 #' @importFrom utils capture.output head modifyList
+#' @importFrom yaml read_yaml
+#' @importFrom ggrepel geom_text_repel
+#' @importFrom ggnewscale new_scale_color
+#' @importFrom kableExtra kbl
+#' @importFrom scales percent
+#' @importFrom tidyr pivot_longer
+#' @importFrom digest digest
+#' @importFrom colorspace lighten
+#' @importFrom doFuture registerDoFuture
 NULL
